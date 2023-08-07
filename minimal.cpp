a460013d8e14931d1afbf9da5e9478e356a4ca68
Rahul Verma <33773797+vrintle@users.noreply.github.com>
2023-08-07 12:38:19 +0530

    Create minimal.cpp

---
+++ minimal.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int int64_t
+#define endl '\n'
+#define sz(v) ((int) v.size())
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define pii pair<int, int>
+#define inf 2e18
+
+int max(int x, int y) { return x > y ? x : y; }
+int min(int x, int y) { return x < y ? x : y; }
+
+void solve() {
+	
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t = 1;
+	cin >> t;
+	while(t--) {
+		solve();
+		cout << endl;
+	}
+	return 0;
+}
