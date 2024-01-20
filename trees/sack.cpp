3dc07297783949e845775133f2757285484a185f
Rahul Verma <33773797+vrintle@users.noreply.github.com>
2024-01-20 20:45:50 +0530

    Update sack.cpp

--- trees/sack.cpp
+++ trees/sack.cpp
@@ -1,178 +1,105 @@
-//////////////////////////////////////////////////////////////////
-/////                                                           //
-////  Credits: Rahul Verma (CC @vrintle, CF @BlindingKnight)   ///
-///  Institution: Delhi Technological University (aka. DCE)   ////
-//                                                           /////
-//////////////////////////////////////////////////////////////////
+// Author: vrintle (Rahul Verma)
 
-#include <chrono>
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace chrono;
 using namespace __gnu_pbds;
 
-template <typename T> using pbds = tree<T, null_type,
-	less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-// find_by_order(K): Returns an iterator to the Kth largest element (counting from zero)
-// order_of_key (K): Returns the number of items that are strictly smaller than K
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-#define F first
-#define S second
-#define db double
-#define ld long db
-#define M 1000000007
-#define pb push_back
-#define ll long long
-#define lll __int128
-#define vl vector<ll>
-#define vd vector<db>
-#define vld vector<ld>
-#define vvl vector<vl>
-#define pl pair<ll,ll>
-#define pd pair<db,db>
-#define vpl vector<pl>
-#define vpd vector<pd>
-#define lld __float128
-#define ull unsigned ll
-#define sz(a) ((ll) a.size())
-#define PI 3.141592653589793238
+#define int int64_t
+#define endl '\n'
+#define sz(v) ((int) v.size())
 #define all(v) v.begin(), v.end()
-#define each(seq) for(auto e: seq)
-#define inf numeric_limits<ll>::max()
-#define acc(a) accumulate(all(a), 0ll)
-#define shuff(a) random_shuffle(all(a))
-#define F0(n, i) for(ll i = 0; i < n; i++)
-#define F1(n, i) for(ll i = 1; i <= n; i++)
-#define google cout << "Case #" << i << ": ";
-#define dbgpr(pr) cout << ' ' << #pr << " {" << pr.F << ',' << pr.S << "} "
-#define dbgprs(seq) cout << #seq << " <"; each(seq) { dbgpr(e); } cout << ">\n"
-#define dbgarr(seq) cout << #seq << " < "; each(seq) { cout << e << ' '; } cout << ">\n"
-#define dbgmtx(mat) cout << #mat << " {\n"; each(mat) { cout << ' '; dbgarr(e); } cout << "}\n"
-#define lrot(a, l, r, k) rotate(a.begin() + l, a.begin() + l + (k % (r - l + 1)), a.begin() + r + 1)
-#define rrot(a, l, r, k) rotate(a.begin() + l, a.begin() + r + 1 - (k % (r - l + 1)), a.begin() + r + 1)
-#define dbgmap(hash) cout << #hash << " { "; each(hash) { cout << e.first << ':' << e.second << ' '; } cout << "}\n"
-#define dbgtree(tree) cout << #tree << " {\n"; each(tree) { cout << e.first << ": "; dbgarr(e.second); } cout << "}\n"
+#define vi vector<int>
+#define pii pair<int, int>
+#define inf 2e18
 
-// Debug Template, copied from Mikel_Arteta_8 (https://codeforces.com/blog/entry/68809)
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifdef VRINTLE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define debug(x...)
-#endif
-// End of debugging template
+int max(int x, int y) { return x > y ? x : y; }
+int min(int x, int y) { return x < y ? x : y; }
 
-/* ostream& operator<<(ostream& o, const __int128& x) {
-    if (x == numeric_limits<__int128>::min()) return o << "-170141183460469231731687303715884105728";
-    if (x < 0) return o << "-" << -x;
-    if (x < 10) return o << (char)(x + '0');
-    return o << x / 10 << (char)(x % 10 + '0');
-} */
-
-ll sieve_size = 0, facto_size = 0;
-vector<ll> F(facto_size+1);
-vector<bool> state(sieve_size+1, 1);
-// exp() for (N^K)%M is only valid, when N%M != 0, else return 0
-ll exp(ll n,ll k,ll m=M){ll r=1,a=n%m;do{r=k&1?r*a%m:r;a=a*a%m;}while(k/=2);return r;}
-ll imod(ll a,ll m=M){return exp(a,m-2,m)%m;}
-ll nCr(ll n,ll r,ll m=M){return r?(F[n]*imod(F[r],m)%m*imod(F[n-r],m)%m)%m:1;}
-void facto(ll n=facto_size,ll m=M){F[0]=1;F1(n,i)F[i]=(F[i-1]*i)%m;}
-void sieve(ll n=sieve_size){for(ll i=4;i<=n;i+=2)state[i]=0;for(ll i=3;i<=n;i+=2)for(ll j=i*i;j<=n;j+=i)state[j]=0;}
-
-const int N = 5e5 + 7;
-map<ll, ll> cnt[N], tot;
-vpl g[N];
-ll a[N], wgt[N];
+const int N = 2e5 + 7;
+vi g[N];
+pbds<int>* s[N];
+int tin[N], tout[N];
 
 void solve() {
-	ll n;
+	int n;
 	cin >> n;
-	tot.clear();
-	F0(n, i) {
-		cin >> a[i];
-		wgt[i] = 0;
-		g[i].clear();
-		tot[a[i]]++;
-		cnt[i].clear();
+	for(int _ = 0; _ < n - 1; _++) {
+		int u, v;
+		cin >> u >> v;
+		u--; v--;
+		g[u].push_back(v);
+		g[v].push_back(u);
 	}
-	function<void(ll, ll, ll)> dfs = [&](ll i, ll p, ll j) -> void {
-		ll mx = 0, big = i, eg = -1;
-		for(auto& [e, x]: g[i]) {
-			if(e != p) {
-				dfs(e, i, x);
-				if(sz(cnt[e]) > mx) mx = sz(cnt[e]), big = e, eg = x;
+	int t = 0;
+	function<void(int, int)> dfs0 = [&](int i, int p) -> void {
+		tin[i] = t++;
+		for(auto& v: g[i]) {
+			if(v != p) {
+				dfs0(v, i);
 			}
 		}
-		if(j == -1) return ;
-		if(big != i) {
-			swap(cnt[i], cnt[big]);
-			wgt[j] = wgt[eg];
+		tout[i] = t++;
+	};
+	dfs0(0, -1);
+	vi pfx(n * 2);
+	function<void(int, int)> dfs = [&](int i, int p) -> void {
+		s[i] = new pbds<int>();
+		int mx = 0, big = i;
+		for(auto& e: g[i]) {
+			if(e != p) {
+				dfs(e, i);
+				if((int) s[e]->size() > mx) mx = s[e]->size(), big = e;
+			}
 		}
-		for(auto& [e, k]: g[i]) {
+		if(big != i) {
+			int r = s[big]->order_of_key(i);
+			pfx[0] += r; pfx[tin[big]] -= r;
+			pfx[tout[big]] += r; pfx[2 * n - 1] -= r;
+			s[i] = s[big];
+		}
+		s[i]->insert(i);
+		for(auto& e: g[i]) {
 			if(e != p && e != big) {
-				for(auto& [k, v]: cnt[e]) {
-					if(cnt[i].find(k) == cnt[i].end()) {
-						if(v < tot[k]) wgt[j] += k;
-						cnt[i][k] += v;
-					} else {
-						if(cnt[i][k] + v == tot[k]) wgt[j] -= k;
-						cnt[i][k] += v;
-					}
+				int r = s[e]->order_of_key(i);
+				pfx[0] += r; pfx[tin[e]] -= r;
+				pfx[tout[e]] += r; pfx[2 * n - 1] -= r;
+				for(auto it = s[e]->begin(); it != s[e]->end(); it++) {
+					s[i]->insert(*it);
 				}
 			}
 		}
-		if(cnt[i].find(a[i]) == cnt[i].end()) {
-			if(1 < tot[a[i]]) wgt[j] += a[i];
-			cnt[i][a[i]] += 1;
-		} else {
-			if(cnt[i][a[i]] + 1 == tot[a[i]]) wgt[j] -= a[i];
-			cnt[i][a[i]] += 1;
-		}
+		int out = i - s[i]->order_of_key(i);
+		pfx[tin[i]] += out;
+		pfx[tout[i]] -= out;
 	};
-	F0(n - 1, i) {
- 		ll u, v;
-		cin >> u >> v;
-		u--; v--;
-		g[u].pb({v, i});
-		g[v].pb({u, i});
+	dfs(0, -1);
+	for(int i = 1; i < n * 2; i++) {
+		pfx[i] += pfx[i - 1];
 	}
-	dfs(0, -1, -1);
-	F0(n - 1, i) cout << wgt[i] << ' ';
+	for(int i = 0; i < n; i++) {
+		cout << pfx[tin[i]] << ' ';
+	}
 }
 
+// PRE-SUBMIT CHECKLIST
+// --------------------
+//
+// -- reset the global arrays
+// -- sort vector of vectors carefully
+// more to be added...
+
 int32_t main() {
-	auto start = high_resolution_clock::now();
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	ll t = 1;
-	cin >> t;
-	F1(t, i) {
-		// google
+	int t = 1;
+	// cin >> t;
+	while(t--) {
 		solve();
-		cout << '\n';
+		cout << endl;
+		// cerr << endl;
 	}
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(stop - start);
-	debug(duration.count(), " µs!");
 	return 0;
 }
